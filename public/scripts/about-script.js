d64d01d6ac4555650d1ac790e530656fce9bcd30
Qasem Al-Aqrabi <152648064+QasemMagdi2002@users.noreply.github.com>
2023-12-06 22:20:38 +0200

    Delete public directory

--- public/scripts/about-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const returnButton = document.getElementById('returnButton');
-
-  returnButton.addEventListener('click', function () {
-    // Return to the Home page
-    window.location.href = '/';
-  });
-});
