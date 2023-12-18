eb4099128f387bdd2794559f646d85306000d6c8
Qasem Al-Aqrabi <152648064+QasemMagdi2002@users.noreply.github.com>
2023-12-18 16:57:25 +0200

    Delete public directory

--- public/scripts/toggledropdown.js
+++
@@ -1,7 +0,0 @@
-function toggleDropdown(id) {
-    var dropdownContent = document.getElementById(id);
-    var dropdownBtn = document.querySelector('.dropdown-btn-' + id);
-
-    dropdownContent.classList.toggle('show-dropdown');
-    dropdownBtn.classList.toggle('active');
-}
