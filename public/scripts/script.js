b0af81428dcd8cd36aff64e27c760372409409f9
Qasem Al-Aqrabi <152648064+QasemMagdi2002@users.noreply.github.com>
2023-12-25 20:27:38 +0200

    Delete public directory

--- public/scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const redirectButton = document.getElementsByClassName('Sign')[0];
-
-  redirectButton.addEventListener('click', function () {
-    // Redirect to the About page
-    window.location.href = '/signup';
-  });
-});
